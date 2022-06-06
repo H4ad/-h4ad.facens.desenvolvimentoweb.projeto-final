5b745961a440e32bac83a40f0dc90afe89ce29ab
Vinícius Lourenço <vinyygamerlol@gmail.com>
2022-06-06 15:08:28 -0300

    fix(database): problem with connection

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -5,11 +5,7 @@ import { UserEntity } from './entities/user.entity';
 export async function getDatabaseConnection(): Promise<DataSource> {
   const mongoDataSource = new DataSource({
     type: 'mongodb',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    url: process.env.TYPEORM_URL,
     useNewUrlParser: true,
     logger: 'advanced-console',
     logging: true,
